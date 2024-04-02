890261369baa849c556d182d282536fd48f25a23
K99G <114172204+K99G@users.noreply.github.com>
2024-04-02 19:13:27 +0200

    Revert "not working yet"
    This reverts commit b508f51af9391d1d973e76098b26cbcd813b2701.

--- ProMgr/MgrAPI/models/Developer.cs
+++
@@ -1,9 +0,0 @@
-public class Developer
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-
-    // Navigation property
-    public ICollection<ProjectDeveloper> ProjectDevelopers { get; set; }
-}
--- ProMgr/MgrAPI/models/ProjectDeveloper.cs
+++
@@ -1,8 +0,0 @@
-public class ProjectDeveloper
-{
-    public int DeveloperId { get; set; }
-    public Developer Developer { get; set; }
-
-    public int ProjectId { get; set; }
-    public Project Project { get; set; }
-}
