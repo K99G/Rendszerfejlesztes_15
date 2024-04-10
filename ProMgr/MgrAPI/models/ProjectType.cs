3e8069b5a8a6a664ddeaa3fd6599e09295cdba8e
Norbert <GJELRR@student.uni-pannon.hu>
2024-04-10 20:59:58 +0200

    Folder CLEAN-UP
    Deleted old folders

--- ProMgr/MgrAPI/models/ProjectType.cs
+++
@@ -1,8 +0,0 @@
-public class ProjectType
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-
-    // Navigation property
-    public ICollection<Project> Projects { get; set; }
-}
